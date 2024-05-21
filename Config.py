43d38482ba6bd41289f224bc1f02d16f5d55e109
nschubi <dev@schulzebilk.de>
2024-05-21 20:35:53 +0200

    lagere Config in eigenes File aus

---
+++ Config.py
@@ -0,0 +1,29 @@
+#Parameter Simulation
+BEES_SCOUT = 10 #Anzahl Scout Bienen im System zum Start
+BEES_EMPLOYED = 0 #Anzahl Employed Bienen im System zum Start
+BEES_ONLOOKER = 200 #Anzahl Onlooker Bienen im System zum Start
+MAX_BEES_SCOUT = 5 #Anzahl maximale Scout Bienen im System
+MAX_BEES_DANCER = 5 #Anzahl maximale tanzende Bienen
+FOOD_COUNT = 100 #Anzahl Futterquellen im System
+MAX_SUGAR = 7 #Maximale Anzahl Zuckereinheiten pro Futterquelle
+MIN_SUGAR = 1 #Minimale Anzahl an Zuckereinheiten pro Futterquelle
+MAX_UNITS = 100 #Maximale Futterkapazität einer Futterquelle
+MIN_UNITS = 15 #Minimale Futterkapazität einer Futterquelle
+MAX_TIME = 1800000   #86.400 #Anzahl Zeit der Simulation
+MIN_RANGE_FOOD_TO_HIVE = 150 #Wie weit müssen Futterquellen mindestens vom Bienenstock entfernt sein
+
+#Paramter Biene
+MAX_VELOCITY_BEE = 100 #Maximale Geschwindigkeit einer Biene
+MIN_VELOCITY_BEE = 80 #Minimale Geschwindigkeit einer Biene
+BEE_VISION = 30 #Sichtradius einer Biene
+BEE_MAX_CAPACITY = 2 #Maximale Anzahl Futter das eine Biene tragen kann
+REDUCE_SPEED_WHEN_CARRY = 15 #Reduktion der Geschwindigkeit wenn die Biene Nahrung trägt
+MAX_STEP_COUNTER_BEES = 1000 #Anzahl Schritte bevor die Biene zurückkehren muss
+
+#Paramter Schwänzeltanz
+MAX_BEES_TO_EMPLOY = 5 #Anzahl an Bienen die von einer Biene rekrutiert werden können
+MAX_DANCE_COUNTER = 100 #Dauer des Schwänzeltanz
+
+# Bildschirmgröße
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 800
