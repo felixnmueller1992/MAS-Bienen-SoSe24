992adafbc166f58458c95edf7808988e9fc3fffc
nschubi <dev@schulzebilk.de>
2024-05-25 18:36:18 +0200

    nutze Sprite superclass für Hive

--- Hive.py
+++ Hive.py
@@ -4,16 +4,27 @@ from Color import *
 
 
 # Klasse Bienenstock
-class Hive:
+class Hive(pygame.sprite.Sprite):
     def __init__(self, x, y):
+        super().__init__()
         self.x = x
         self.y = y
         self.food_count = 0  # Anzahl Nahrung im Bienenstock
         self.scout_bees = 0  # Anzahl Scout Bienen dem Bienenstock zugewiesen
         self.dance_bees = 0  # Anzahl tanzende Bienen
 
-    def draw(self, screen):  # Zeichne den Bienenstock
-        pygame.draw.circle(screen, BLACK, (self.x, self.y), 25)
+        self.image = pygame.Surface((50, 50), pygame.SRCALPHA)
+        self.image.fill((0, 0, 0, 0))
+        pygame.draw.circle(self.image, BLACK, (25, 25), 25)
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    # def draw(self, screen):  # Zeichne den Bienenstock
+    #     pygame.draw.circle(screen, BLACK, (self.x, self.y), 25)
 
     def deliver(self, food_amount, sugar_amount):  # Nahrung wird an Bienenstock übergeben
         self.food_count = self.food_count + (food_amount * sugar_amount)
+
+    def update(self):
+        pass
--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ from Hive import Hive
 from Bee import Bee
 from Foodsource import Foodsource
 
+
 # Hauptfunktion
 def main():
     pygame.init()
@@ -16,17 +17,25 @@ def main():
     pygame.display.set_caption("Bienenstock Simulation")
     clock = pygame.time.Clock()
 
-    hive = Hive(SCREEN_WIDTH / random.randint(2, 4),
-                SCREEN_HEIGHT / random.randint(2, 4))  # Erzeugt Biennestock und Bienen
-    bees = [Bee(0, hive.x, hive.y) for _ in range(BEES_SCOUT)]  # Erzeugt Scout Bienen (Status = 0)
-    bees = bees + [Bee(1, hive.x, hive.y) for _ in range(BEES_EMPLOYED)]  # Erzeugt employed Bienen (Status = 1)
-    bees = bees + [Bee(2, hive.x, hive.y) for _ in range(BEES_ONLOOKER)]  # Erzeugt Onlooker Bienen (Status = 2)
+    # Initialisiere alle Objekte für Simulation
+    hivePosX = SCREEN_WIDTH / random.randint(2, 4)
+    hivePosY = SCREEN_HEIGHT / random.randint(2, 4)
+
+    # Erzeuge Bienenstock
+    hive = Hive(hivePosX, hivePosY)
+    hive_group = pygame.sprite.Group()
+    hive_group.add(hive)
+
+    bees = [Bee(0, hivePosX, hivePosY) for _ in range(BEES_SCOUT)]  # Erzeugt Scout Bienen (Status = 0)
+    bees = bees + [Bee(1, hivePosX, hivePosY) for _ in range(BEES_EMPLOYED)]  # Erzeugt employed Bienen (Status = 1)
+    bees = bees + [Bee(2, hivePosX, hivePosY) for _ in range(BEES_ONLOOKER)]  # Erzeugt Onlooker Bienen (Status = 2)
+
     foods = [
         Foodsource(random.randint(MIN_UNITS, MAX_UNITS), random.randint(MIN_SUGAR, MAX_SUGAR), MIN_RANGE_FOOD_TO_HIVE,
-                   hive.x, hive.y) for _ in range(FOOD_COUNT)]
-    total_food_amount = 0  #Gesamte verfügbare Futter Einheiten
+                   hivePosX, hivePosY) for _ in range(FOOD_COUNT)]
+    total_food_amount = 0  # Gesamte verfügbare Futter Einheiten
 
-    #Alle Futterquellen zusammenzählen
+    # Alle Futterquellen zusammenzählen
     for foodsource in foods:
         total_food_amount = total_food_amount + (
                 foodsource.units * foodsource.sugar)  # Gesamte Futtereinheiten zusammenzählen
@@ -37,40 +46,44 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        if pygame.time.get_ticks() > MAX_TIME:  #Simulation nach abgelaufener Zeit beenden
+        if pygame.time.get_ticks() > MAX_TIME:  # Simulation nach abgelaufener Zeit beenden
             running = False
 
-        screen.fill(WHITE)  #Hintergrund zeichnen
-        hive.draw(screen)  #Bienenstock auf Karte zeichnen
+        screen.fill(WHITE)  # Hintergrund zeichnen
+        hive_group.draw(screen)  # Bienenstock auf Karte zeichnen
+        hive_group.update()
 
-        #Alle Futterquellen updaten
+        # Alle Futterquellen updaten
         for foodsource in foods:
             foodsource.draw(screen)
 
-        hive.scout_bees = 0  # Anzahl Scout Bienen intialisieren
-        hive.dance_bees = 0  #Anzahl Tanzende Bienen initialisieren
-        for bee in bees:  # Alle Bienen zählen
-            if bee.occupation == 0:  #Biene ist Scout Biene
-                hive.scout_bees += 1  #Anzahl Scout Bienen im Bienenstock
-            if bee.occupation == 5:  #Biene ist tanzende Biene
-                hive.dance_bees += 1  #Anzahl Tanzende Bienen im Bienenstock
+        # TODO Bienen werden nicht mehr zurückgesetzt! Nur temporär!
+        # hive.scout_bees = 0  # Anzahl Scout Bienen intialisieren
+        # hive.dance_bees = 0  # Anzahl Tanzende Bienen initialisieren
+        # for bee in bees:  # Alle Bienen zählen
+        #     if bee.occupation == 0:  # Biene ist Scout Biene
+        #         hive.scout_bees += 1  # Anzahl Scout Bienen im Bienenstock
+        #     if bee.occupation == 5:  # Biene ist tanzende Biene
+        #         hive.dance_bees += 1  # Anzahl Tanzende Bienen im Bienenstock
 
-        for bee in bees:  #Alle Bienen updaten
+        for bee in bees:  # Alle Bienen updaten
             bee.update()
             bee.draw(screen)
             if bee.occupation == 5:  # Biene ist tanzende Biene
-                for onlooker in bees:  #Schleife um Bienen in der nähe der tanzen Biene zu finden
+                for onlooker in bees:  # Schleife um Bienen in der nähe der tanzen Biene zu finden
                     if onlooker.occupation == 2 and bee.amount_employed < min(bee.dance_information[2],
                                                                               bee.dance_information[
-                                                                                  3]):  #Biene ist Onlooker und Es dürfen so viele Bienen rekuriert werden, wie der Zuckergehalt der
-                        onlooker.dance_information = bee.dance_information  #Übergabe Tanz Informationen von tanzende Biene zu Onlooker Biene
-                        onlooker.change_occupation(1)  #Biene wird employed
-                        bee.amount_employed = bee.amount_employed + 1  #Zähler für maximale Anzahl Bienen rekrutierbar
-            if bee.occupation == 4:  #Wenn Biene im Stock ist
+                                                                                  3]):  # Biene ist Onlooker und es
+                        # dürfen so viele Bienen rekuriert werden, wie der Zuckergehalt der
+                        onlooker.dance_information = bee.dance_information  # Übergabe Tanz Informationen von
+                        # tanzende Biene zu Onlooker Biene
+                        onlooker.change_occupation(1)  # Biene wird employed
+                        bee.amount_employed = bee.amount_employed + 1  # Zähler für maximale Anzahl Bienen rekrutierbar
+            if bee.occupation == 4:  # Wenn Biene im Stock ist
                 hive.deliver(bee.capacity,
-                             bee.dance_information[2])  #Nahrungsübergabe an Bienenstock und Zuckergehalt übergabe
-                bee.deliver(hive.scout_bees, hive.dance_bees)  #Nahrung von Biene entfernen
-            #Abfrage ob eine Futterquelle im Sichtbereich der Biene liegt
+                             bee.dance_information[2])  # Nahrungsübergabe an Bienenstock und Zuckergehalt übergabe
+                bee.deliver(hive.scout_bees, hive.dance_bees)  # Nahrung von Biene entfernen
+            # Abfrage ob eine Futterquelle im Sichtbereich der Biene liegt
             for food in foods:
                 if food.units > 1 and bee.x > food.x - (food.units + BEE_VISION) and bee.x < food.x + (
                         food.units + BEE_VISION) and bee.y > food.y - (food.units + BEE_VISION) and bee.y < food.y + (
@@ -90,7 +103,7 @@ def main():
         label_surface = label_font.render(label_text, True, BLACK)
         screen.blit(label_surface, (10, 10))
 
-        #Zeichen Label für Information Futterquelle
+        # Zeichen Label für Information Futterquelle
         label_text = "Anzahl Futter gesammelt: " + str(hive.food_count).encode("utf-8").decode("utf-8") + " / " + str(
             total_food_amount).encode("utf-8").decode("utf-8")
         label_surface = label_font.render(label_text, True, BLACK)
@@ -100,15 +113,19 @@ def main():
         label_text = "Scout Biene"
         label_surface = label_font.render(label_text, True, COLOR_BEE_SCOUT)
         screen.blit(label_surface, (10, 700))
+
         label_text = "Employed Biene"
         label_surface = label_font.render(label_text, True, COLOR_BEE_EMPLOYED)
         screen.blit(label_surface, (10, 720))
+
         label_text = "Onlooker Biene"
         label_surface = label_font.render(label_text, True, COLOR_BEE_ONLOOKER)
         screen.blit(label_surface, (10, 740))
+
         label_text = "Biene kehrt zurück"
         label_surface = label_font.render(label_text, True, YELLOW)
         screen.blit(label_surface, (10, 760))
+
         label_text = "Biene Schwänzeltanz"
         label_surface = label_font.render(label_text, True, COLOR_BEE_DANCER)
         screen.blit(label_surface, (10, 780))
