8b308885a029db96289639ccece46f5d8225e48c
felix <felix.n.mueller@web.de>
2024-05-29 10:48:14 +0200

    Legende Zeichnen Funktion aus Main auslagern

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 import random
-
 import pygame
 
 from Color import *
 from Config import *
 from Foodsource import Foodsource
 from Hive import Hive
+from Legende import *
 
 
 # Hauptfunktion
@@ -66,49 +66,12 @@ def main():
         foodsource_group.draw(screen)
         foodsource_group.update()
 
-        legende_zeichnen(screen, hive, total_food_amount)
+        legende_zeichnen(screen, hive, total_food_amount) #Legende auf die Map zeichnen
 
         pygame.display.flip()
         clock.tick(60)
 
     pygame.quit()
-
-
-def legende_zeichnen(screen, hive, total_food_amount):
-    # Schriftart für Labels
-    label_font = pygame.font.SysFont("Arial", 18)
-    label_text = "Anzahl Sekunden vergangen: " + str(pygame.time.get_ticks() / 1000).encode("utf-8").decode("utf-8")
-    # Zeichne Label für die vergangene Zeit
-    label_surface = label_font.render(label_text, True, BLACK)
-    screen.blit(label_surface, (10, 10))
-
-    # Zeichen Label für Information Futterquelle
-    label_text = "Anzahl Futter gesammelt: " + str(hive.food_count).encode("utf-8").decode("utf-8") + " / " + str(
-        total_food_amount).encode("utf-8").decode("utf-8")
-    label_surface = label_font.render(label_text, True, BLACK)
-    screen.blit(label_surface, (10, 30))
-
-    # Zeichen Labels für Legende
-    label_text = "Scout Biene"
-    label_surface = label_font.render(label_text, True, COLOR_BEE_SCOUT)
-    screen.blit(label_surface, (10, 700))
-
-    label_text = "Employed Biene"
-    label_surface = label_font.render(label_text, True, COLOR_BEE_EMPLOYED)
-    screen.blit(label_surface, (10, 720))
-
-    label_text = "Onlooker Biene"
-    label_surface = label_font.render(label_text, True, COLOR_BEE_ONLOOKER)
-    screen.blit(label_surface, (10, 740))
-
-    label_text = "Biene kehrt zurück"
-    label_surface = label_font.render(label_text, True, YELLOW)
-    screen.blit(label_surface, (10, 760))
-
-    label_text = "Biene Schwänzeltanz"
-    label_surface = label_font.render(label_text, True, COLOR_BEE_DANCER)
-    screen.blit(label_surface, (10, 780))
-
 
 if __name__ == "__main__":
     main()
