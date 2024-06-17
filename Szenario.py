f6e8e98328292c4e781db464234a8f3e9bdc5ea5
lukasklug <lukas.klug97@gmail.com>
2024-06-18 00:48:55 +0200

    initiale Version des Szenarien.py Files -> erlaubt export des Szenarios in CSV-Datei

---
+++ Szenario.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import time 
+
+
+
+#from Config import *
+
+
+
+export_file_name = time.strftime("%Y%m%d_%H%M%S_Exportfile.csv")
+
+def export_szenario(environment_data):   
+    df_export = pd.DataFrame(environment_data).T    
+    df_export.to_csv(export_file_name, mode='a',header=False, index=False)
+
+
+            
+    
+    
+
+    
+
+    
