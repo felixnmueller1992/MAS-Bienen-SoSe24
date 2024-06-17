05f4264e4a67f027b9ee2f6bccb5231e3831bc56
lukasklug <lukas.klug97@gmail.com>
2024-06-18 00:47:56 +0200

    Hinzufügen der Funktionen für den Datei Import und Export von Szenarien

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from Config import *
 from Foodsource import Foodsource
 from Hive import Hive
 from Legende import *
+from Szenario import *
 
 
 # Hauptfunktion
@@ -17,31 +18,61 @@ def main():
     clock = pygame.time.Clock()
 
     # Initialisiere alle Objekte für Simulation
-    hivePosX = SIMULATION_WIDTH / random.randint(2, 4)
-    hivePosY = SCREEN_HEIGHT / random.randint(2, 4)
 
-    # Erzeuge Bienenstock
-    hive = Hive(hivePosX, hivePosY)
-    hive_group = pygame.sprite.Group()
-    hive_group.add(hive)
+    if len(IMPORT_ENVIRONMENT_FILE) > 0:       # Falls file import gewünscht
+        hive_group = pygame.sprite.Group()
+        foodsource_group = pygame.sprite.Group()
+
+        df_import = pd.read_csv(IMPORT_ENVIRONMENT_FILE, names=['Object', 'Units', 'Sugar', 'X_cord', 'Y_cord'])
+        for index, row in df_import.iterrows():
+            if row['Object'] == "Hive":
+                hive = Hive(row['X_cord'], row['Y_cord'])
+                hive_group.add(hive)
+
+            if row['Object'] == "Foodsource":
+                food = Foodsource(row['Units'], row['Sugar'], row['X_cord'], row['Y_cord']) 
+                foodsource_group.add(food)
+        
+    else:                                       # Zufällige initialisierung falls kein file import gewünscht
+        
+        hivePosX = SIMULATION_WIDTH / random.randint(2, 4)
+        hivePosY = SCREEN_HEIGHT / random.randint(2, 4)
+
+        # Erzeuge Bienenstock
+        hive = Hive(hivePosX, hivePosY)
+        hive_group = pygame.sprite.Group()
+        hive_group.add(hive)
+
+        # Erzeuge Futterquellen
+        foodsource_group = pygame.sprite.Group()
+        foods = [
+            Foodsource(random.randint(MIN_UNITS, MAX_UNITS), random.randint(MIN_SUGAR, MAX_SUGAR), MIN_RANGE_FOOD_TO_HIVE,
+                    hivePosX, hivePosY) for _ in range(FOOD_COUNT)]
+        foodsource_group.add(foods)
+
 
     # Erzeuge Bienen
     bees = hive.create_bees()
     bee_group = pygame.sprite.Group()
     bee_group.add(bees)
 
-    # Erzeuge Futterquellen
-    foodsource_group = pygame.sprite.Group()
-    foods = [
-        Foodsource(random.randint(MIN_UNITS, MAX_UNITS), random.randint(MIN_SUGAR, MAX_SUGAR), MIN_RANGE_FOOD_TO_HIVE,
-                   hivePosX, hivePosY) for _ in range(FOOD_COUNT)]
-    foodsource_group.add(foods)
     total_food_amount = 0
 
-    # Alle Units der Futterquellen zusammenzählen
+        # Alle Units der Futterquellen zusammenzählen
     for foodsource in foodsource_group:
-        total_food_amount = total_food_amount + (
-                foodsource.units * foodsource.sugar)
+        total_food_amount = total_food_amount + (foodsource.units * foodsource.sugar)
+
+
+    if EXPORT_ENVIRONMENT == True:
+
+        for hive in hive_group:
+            environment_data = ["Hive", "N/A" , "N/A", hive.x, hive.y]
+            export_szenario(environment_data)
+
+        for foodsource in foodsource_group:
+            environment_data = ["Foodsource",foodsource.units, foodsource.sugar, foodsource.x, foodsource.y]
+            export_szenario(environment_data)
+            
 
     # Hauptschleife
     running = True
