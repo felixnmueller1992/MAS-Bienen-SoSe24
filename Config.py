81e47b88ed713a5dc05e313fc94f65c15cbed5c1
nschubi <dev@schulzebilk.de>
2024-07-01 21:35:06 +0200

    füge neuen Parameter DANCEFLOOR_RADIUS ein

--- Config.py
+++ Config.py
@@ -15,7 +15,7 @@ MIN_RANGE_FOOD_TO_HIVE = 150  # Wie weit müssen Futterquellen mindestens vom Bi
 # Parameter Biene
 MAX_VELOCITY_BEE = 200  # Maximale Geschwindigkeit einer Biene
 MIN_VELOCITY_BEE = 160  # Minimale Geschwindigkeit einer Biene
-WALKING_SPEED = 0.4 # Faktor für die Gehgeschwindigkeit einer Biene
+WALKING_SPEED = 0.4  # Faktor für die Gehgeschwindigkeit einer Biene
 BEE_VISION = 30  # Sichtradius einer Biene
 BEE_MAX_CAPACITY = 2  # Maximale Anzahl Futter das eine Biene tragen kann
 REDUCE_SPEED_WHEN_CARRY = 15  # Reduktion der Geschwindigkeit, wenn die Biene Nahrung trägt
@@ -24,6 +24,7 @@ MAX_STEP_COUNTER_BEES = 1000  # Anzahl Schritte bevor die Biene zurückkehren mu
 # Parameter Schwänzeltanz
 MAX_BEES_TO_EMPLOY = 5  # Anzahl an Bienen die von einer Biene rekrutiert werden können
 MAX_DANCE_COUNTER = 100  # Dauer des Schwänzeltanz
+DANCEFLOOR_RADIUS = 8  # Radius einer einzelnen Tanzfläche
 
 # Bildschirmgröße
 SCREEN_HEIGHT = 800
