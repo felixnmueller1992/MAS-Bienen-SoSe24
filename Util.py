21bd811829424378ca0e154960cd938f65870422
lukasklug <lukas.klug97@gmail.com>
2024-07-19 06:50:31 -0300

    Hinzufügen von Funktion collect_config_data

--- Util.py
+++ Util.py
@@ -1,2 +1,12 @@
+import pandas as pd
+
+from Config import *
+
+
 def interpolate(x, lowest, highest, min_value=0, max_value=1):
     return min_value + (max_value - min_value) * (x - lowest) / (highest - lowest)
+
+def collect_config_data():
+    
+    config_df = pd.read_table("Config.py", header=None)
+    return config_df
